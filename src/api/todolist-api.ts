d45870907f9b29050cf9c5743b368ca5769d1206
ivan kevra <vanyakevra@gmail.com>
2023-04-29 20:46:26 +0300

    new commit

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -1,102 +1,102 @@
-import axios from "axios";
-import {TaskType} from "../features/todolistsList/tasks-reducer";
-
-const instance = axios.create({
-    baseURL: 'https://social-network.samuraijs.com/api/1.1/',
-    withCredentials: true,
-    headers: {
-        // Не забываем заменить API-KEY на собственный
-        'API-KEY': '9a6c26a3-7c0a-407d-ba38-123ea04fe40d',
-    },
-})
-
-//api
-export const todolistAPI = {
-    getTodolists() {
-        return instance.get<Array<TodolistType>>('todo-lists')
-    },
-    createTodolist(title: string) {
-        return instance.post<ResponseType<TodolistType>>('todo-lists', {title})
-    },
-    deleteTodolist(todolistId: string) {
-        return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
-    },
-    updateTodolist(todolistId: string, title: string) {
-        return instance.put<ResponseType<{ item: TodolistType }>>(`todo-lists/${todolistId}`, {title})
-    },
-    getTasks(todolistId: string) {
-        return instance.get<GetTasksResponseType>(`/todo-lists/${todolistId}/tasks`)
-    },
-    createTask(todolistId: string, title: string) {
-        return instance.post<ResponseType<TaskType>>(`/todo-lists/${todolistId}/tasks`, {title})
-    },
-    deleteTask(todolistId: string, taskId: string) {
-        return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
-    },
-    updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
-        return instance.put<ResponseType<{ item: TaskType }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
-    }
-}
-
-export const authAPI = {
-    login(data: LoginParamsType) {
-        return instance.post<ResponseType<{ userId?: number }>>('/auth/login', data)
-    },
-    logOut() {
-        return instance.delete<ResponseType<{ userId?: number }>>('/auth/login')
-    },
-    me() {
-        return instance.get<ResponseType<{ id: number, email: string, login: string }>>('/auth/me')
-    }
-}
-
-//types
-export type TodolistType = {
-    id: string
-    title: string
-    addedDate: string
-    order: number
-}
-
-export enum TaskStatuses {
-    New = 0,
-    InProgress = 1,
-    Completed = 2,
-    Draft = 3
-}
-
-export enum TaskPriorities {
-    Low = 0,
-    Middle = 1,
-    Hi = 2,
-    Urgently = 3,
-    Later = 4
-}
-
-export type ResponseType<T = {}> = {
-    data: {
-        item: T
-    }
-    messages: Array<string>
-    fieldsErrors: Array<string>
-    resultCode: number
-}
-type GetTasksResponseType = {
-    items: Array<TaskType>
-    totalCount: number
-    error: string | null
-}
-export type UpdateTaskModelType = {
-    title: string
-    description: string
-    status: TaskStatuses
-    priority: TaskPriorities
-    startDate: string
-    deadline: string
-}
-export type LoginParamsType = {
-    email: string
-    password: string
-    rememberMe: boolean
-    captcha?: string
-}
+// import axios from "axios";
+// import {TaskType} from "../features/todolistsList/tasks-reducer";
+//
+// const instance = axios.create({
+//     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
+//     withCredentials: true,
+//     headers: {
+//         // Не забываем заменить API-KEY на собственный
+//         'API-KEY': '9a6c26a3-7c0a-407d-ba38-123ea04fe40d',
+//     },
+// })
+//
+// //api
+// export const todolistAPI = {
+//     getTodolists() {
+//         return instance.get<Array<TodolistType>>('todo-lists')
+//     },
+//     createTodolist(title: string) {
+//         return instance.post<ResponseType<TodolistType>>('todo-lists', {title})
+//     },
+//     deleteTodolist(todolistId: string) {
+//         return instance.delete<ResponseType>(`todo-lists/${todolistId}`)
+//     },
+//     updateTodolist(todolistId: string, title: string) {
+//         return instance.put<ResponseType<{ item: TodolistType }>>(`todo-lists/${todolistId}`, {title})
+//     },
+//     getTasks(todolistId: string) {
+//         return instance.get<GetTasksResponseType>(`/todo-lists/${todolistId}/tasks`)
+//     },
+//     createTask(todolistId: string, title: string) {
+//         return instance.post<ResponseType<TaskType>>(`/todo-lists/${todolistId}/tasks`, {title})
+//     },
+//     deleteTask(todolistId: string, taskId: string) {
+//         return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`)
+//     },
+//     updateTask(todolistId: string, taskId: string, model: UpdateTaskModelType) {
+//         return instance.put<ResponseType<{ item: TaskType }>>(`/todo-lists/${todolistId}/tasks/${taskId}`, model)
+//     }
+// }
+//
+// export const authAPI = {
+//     login(data: LoginParamsType) {
+//         return instance.post<ResponseType<{ userId?: number }>>('/auth/login', data)
+//     },
+//     logOut() {
+//         return instance.delete<ResponseType<{ userId?: number }>>('/auth/login')
+//     },
+//     me() {
+//         return instance.get<ResponseType<{ id: number, email: string, login: string }>>('/auth/me')
+//     }
+// }
+//
+// //types
+// export type TodolistType = {
+//     id: string
+//     title: string
+//     addedDate: string
+//     order: number
+// }
+//
+// export enum TaskStatuses {
+//     New = 0,
+//     InProgress = 1,
+//     Completed = 2,
+//     Draft = 3
+// }
+//
+// export enum TaskPriorities {
+//     Low = 0,
+//     Middle = 1,
+//     Hi = 2,
+//     Urgently = 3,
+//     Later = 4
+// }
+//
+// export type ResponseType<T = {}> = {
+//     data: {
+//         item: T
+//     }
+//     messages: Array<string>
+//     fieldsErrors: Array<string>
+//     resultCode: number
+// }
+// type GetTasksResponseType = {
+//     items: Array<TaskType>
+//     totalCount: number
+//     error: string | null
+// }
+// export type UpdateTaskModelType = {
+//     title: string
+//     description: string
+//     status: TaskStatuses
+//     priority: TaskPriorities
+//     startDate: string
+//     deadline: string
+// }
+// export type LoginParamsType = {
+//     email: string
+//     password: string
+//     rememberMe: boolean
+//     captcha?: string
+// }
