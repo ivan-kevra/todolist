f1284d557f6f5c74226e04669e71e5ad858be61f
ivan kevra <vanyakevra@gmail.com>
2023-05-30 10:16:46 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
-import {store} from './app/store'
-import {Provider} from 'react-redux'
-import {App} from "./app/App";
 import {createRoot} from "react-dom/client";
-import {BrowserRouter} from "react-router-dom";
+import {App} from "./App";
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container);
 root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>
+    <App/>
 );
